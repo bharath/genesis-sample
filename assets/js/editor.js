c59321e744a919db4c925095b92e438175ec7d56
Bharath <obharath@gmail.com>
2019-06-28 15:19:53 +0530

    cleanup

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,18 +1,19 @@
 wp.domReady( () => {
 
-    wp.blocks.unregisterBlockStyle( 'core/button', 'default' );
-	wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
-	wp.blocks.unregisterBlockStyle( 'core/button', 'squared' );
+    //wp.blocks.unregisterBlockStyle( 'core/button', 'default' );
+	//wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
+	//wp.blocks.unregisterBlockStyle( 'core/button', 'squared' );
 
-    wp.blocks.registerBlockStyle( 'core/button', {
-		name: 'default',
-		label: 'Default',
-        isDefault: true,
-	});
+    //wp.blocks.registerBlockStyle( 'core/button', {
+		//name: 'default',
+		//label: 'Default',
+        //isDefault: true,
+	//});
 	
 	wp.blocks.registerBlockStyle( 'core/button', {
-		name: 'theme-default',
-		label: 'Theme Default',
+		name: 'bk-child-default',
+		label: 'BK Child Default',
+        //isDefault: true,
 	} );
 
 } );
