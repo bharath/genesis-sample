9566ec3b1c81de17437ee34b869997da5628ab97
Bharath <obharath@gmail.com>
2019-07-24 14:59:37 +0530

    Add custom classes to button blocks inner

--- custom-functions.php
+++ custom-functions.php
@@ -435,3 +435,46 @@ function genesis_sample_block_categories( $categories ) {
 		)
 	);
 }
+
+
+add_filter( 'render_block', 'genesis_sample_custom_button_classes', 5, 2 );
+/**
+ * Add Custom classes to Button block link.
+ *
+ * @param array $block_content remove allowed button link classes from the button container first.
+ * @param array $block button block.
+ */
+function genesis_sample_custom_button_classes( $block_content, $block ) {
+
+	if ( 'core/button' === $block['blockName'] && isset( $block['attrs']['className'] ) ) {
+
+		// Setting up a subset of custom button link classes.
+		$allowed_button_link_classes = array(
+			'fancybox',
+			'another-custom-class',
+			'example-custom-class',
+			// ...
+		);
+
+		// Remove allowed button link classes from the button container first.
+		$block_content = str_replace(
+			$allowed_button_link_classes,
+			'',
+			$block_content
+		);
+
+		// Get custom button classes set for the block.
+		$custom_classes = explode( ' ', $block['attrs']['className'] );
+
+		// Apply allowed custom button link classes.
+		$block_content = str_replace(
+			'wp-block-button__link',
+			'wp-block-button__link ' . implode( ' ', array_intersect( $custom_classes, $allowed_button_link_classes ) ),
+			$block_content
+		);
+
+	}
+
+	return $block_content;
+
+}
